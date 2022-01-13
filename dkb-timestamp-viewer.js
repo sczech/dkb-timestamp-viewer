e2944f39cdf44b886a69918291c85538e46940a3
Simon Czech <mail@simonczech.com>
2022-01-13 16:57:04 +0100

    added timestamps for transaction overview page

--- dkb-timestamp-viewer.js
+++ dkb-timestamp-viewer.js
@@ -26,20 +26,51 @@ new MutationObserver(() => {
 function onUrlChange() {
     $(document).ready(function() {
         //Check if we are on a account transaction page
-        const url_regex = new RegExp("https://banking.beta.dkb.de/account/(.+)/trx/(.+)");
-        if (url_regex.test(location.href)) {
+        const url_regex_detail = new RegExp("^https://banking.beta.dkb.de/account/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/trx/(.+)");
+        if (url_regex_detail.test(location.href)) {
             //define RegEx to filter timestamp from URL
             const time_regex = new RegExp("[0-9]{2}[.][0-9]{2}[.][0-9]{2}");
             //retreive timestamp from url and make it more readable (replace dots with colons)
             let time = location.href.match(time_regex)[0].replaceAll('.', ':');
+            //define booking date div
+            const booking_date_div = document.getElementById("main-container").children.item(0).children.item(1).children.item(0).children.item(0).children.item(0).children.item(0).children.item(1).children.item(0).children.item(0).children.item(0).children.item(1)
             //get already existing date information from div
-            let date = document.getElementById("main-container").children.item(0).children.item(1).children.item(0).children.item(0).children.item(0).children.item(0).children.item(1).children.item(0).children.item(0).children.item(0).children.item(1).innerHTML;
+            let date = booking_date_div.innerHTML;
             //make a new string from website data and newly retreived timestamp
             let datetime = date + " " + time;
 
             //write new string to div
-            document.getElementById("main-container").children.item(0).children.item(1).children.item(0).children.item(0).children.item(0).children.item(0).children.item(1).children.item(0).children.item(0).children.item(0).children.item(1).innerHTML = datetime;
+            booking_date_div.innerHTML = datetime;
         }
 
+        //Check if we are on the account transaction overview page
+        const url_regex_overview = new RegExp("^https://banking.beta.dkb.de/account/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$");
+        if (url_regex_overview.test(location.href)) {
+            //map tx list to var
+            const transaction_list = document.getElementById("main-container").children.item(0).children.item(2).children.item(2).children.item(1).children.item(1)
+            //iterate through tx list
+            for (let tx_section of transaction_list.children) {
+                for (let tx_div of tx_section.children) {
+                    //make sure headers stay untouched
+                    if (tx_div.tagName != "HEADER") {
+                        //define needed vars
+                        const tx_href = tx_div.children.item(0).href;
+                        const tx_subtitle = tx_div.children.item(0).children.item(0).children.item(0).children.item(1).children.item(0).children.item(1).children.item(0);
+
+                        //define RegEx to filter timestamp from URL
+                        const time_regex = new RegExp("[0-9]{2}[.][0-9]{2}[.][0-9]{2}");
+                        //retreive timestamp from url, make it more readable (replace dots with colons) and remove seconds
+                        let time = tx_href.match(time_regex)[0].replaceAll('.', ':').slice(0, 5);
+
+                        //split the subtitle string into an array to edit it
+                        let tx_subtitle_array = tx_subtitle.innerHTML.split("•");
+                        //add the time to the first part of the text
+                        tx_subtitle_array[0] = tx_subtitle_array[0] + time + " ";
+                        //join the text back together to a string and assign the value
+                        tx_subtitle.innerHTML = tx_subtitle_array.join("•");
+                    }
+                }
+            }
+        }
     })
 }
